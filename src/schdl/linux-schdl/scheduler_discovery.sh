f56c6a57915efd8f4c9deba66fce4585bd0d5f36
pastranabasaldua12r-afk <pastranabasaldua12r@gmail.com>
2026-01-16 23:07:41 -0600

    Practice: Linux scheduler discovery script

--- src/schdl/linux-schdl/scheduler_discovery.sh
+++ src/schdl/linux-schdl/scheduler_discovery.sh
@@ -33,3 +33,15 @@ ls
 
 # visualiza el contenido del archivo fair.c
 cat fair.c
+
+## Pregunta de reflexión (obligatoria)
+
+Responder dentro del script como comentario:
+
+> ¿Por qué Linux no implementa directamente FCFS, SJF o RR como se ven en los libros?
+# Porque estos algoritmos se presentan de forma teórica para facilitar la comprensión
+# de los conceptos básicos de planificación.
+# En la práctica, un sistema operativo debe administrar muchos procesos al mismo tiempo,
+# con comportamientos y prioridades distintas.
+# Por esta razón, Linux utiliza un planificador más avanzado y flexible, capaz de
+# adaptarse a diferentes situaciones y distribuir el tiempo del CPU de manera más eficiente y justa.
