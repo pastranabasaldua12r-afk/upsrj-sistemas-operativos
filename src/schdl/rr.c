23592805d027ea3b7827506b7f6a8d7c42e0c178
pastranabasaldua12r-afk <pastranabasaldua12r@gmail.com>
2026-01-24 17:13:29 -0600

    Fix RR scheduling and pass all tests

--- src/schdl/rr.c
+++ src/schdl/rr.c
@@ -4,9 +4,12 @@
 /* ============================================================
  * Student implementation area
  * ============================================================ */
+
 void rr_schedule(Process p[], int n, int quantum)
 {
-    int time = 0, completed = 0;
+    int time = 0;
+    int completed = 0;
+
     int remaining[n];
 
     for (int i = 0; i < n; i++)
@@ -14,6 +17,7 @@ void rr_schedule(Process p[], int n, int quantum)
         remaining[i] = p[i].burst_time;
         p[i].waiting_time = 0;
         p[i].turnaround_time = 0;
+        p[i].completed = 0;
     }
 
     while (completed < n)
@@ -26,9 +30,6 @@ void rr_schedule(Process p[], int n, int quantum)
             {
                 executed = 1;
 
-                if (remaining[i] == p[i].burst_time)
-                    p[i].waiting_time = time - p[i].arrival_time;
-
                 if (remaining[i] > quantum)
                 {
                     time += quantum;
@@ -38,17 +39,24 @@ void rr_schedule(Process p[], int n, int quantum)
                 {
                     time += remaining[i];
                     remaining[i] = 0;
-                    p[i].turnaround_time = time - p[i].arrival_time;
+
+                    p[i].turnaround_time =
+                        time - p[i].arrival_time;
+
+                    p[i].waiting_time =
+                        p[i].turnaround_time - p[i].burst_time;
+
+                    p[i].completed = 1;
                     completed++;
                 }
             }
         }
 
+        /* Si nadie pudo ejecutarse, avanzar tiempo */
         if (!executed)
             time++;
     }
 }
-
 
 /* ============================================================
  * DO NOT MODIFY MAIN
